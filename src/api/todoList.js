4ba53af8e030b886518fd3d6f224ad6c08f6dda4
HenryHo <minghang.h@gmail.com>
2022-09-29 15:58:04 +0800

    feature: add comment

--- src/api/todoList.js
+++ src/api/todoList.js
@@ -5,11 +5,21 @@ import {
   deleteDoc,
   addDoc,
 } from 'firebase/firestore';
+
+// initialized db
 import { db } from '../firebase';
 
+/**
+ * firebase v9 document操作的參考文章:
+ * https://softauthor.com/firebase-firestore-add-document-data-using-adddoc/
+ */
+
 export async function getList() {
-  const col = collection(db, 'todoList');
-  const list = await getDocs(col);
+  const col = collection(db, 'todoList'); // 從initialized db中，建立你要操作的collection reference
+  const list = await getDocs(col); // 從這個collection中取回"所有"document
+
+  // 將取回的docs整理成我們需要的格式，其中item.data() 是每一個element的資料
+  // 而item.id是該document的key，這個key是由firebase自動產生的
   const newArray = list.docs.map((item) => ({ id: item.id, ...item.data() }));
 
   return newArray;
